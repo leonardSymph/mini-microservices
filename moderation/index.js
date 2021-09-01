90814cb1e612f4452ed47fb6eb2e650c132c79ed
leonardSymph <leonard@symph.co>
2021-09-02 05:25:25 +0800

    new commits

--- moderation/index.js
+++ moderation/index.js
@@ -5,12 +5,24 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   const { type, data } = req.body;
 
   if (type === 'CommentCreated') {
-    console.log(data);
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
   }
+
+  res.send({});
 });
 
 app.listen(4003, () => {
